2b30fe68b368634b29fb02cc0120bb083b9306b1
Hamid Sayad <hamidsayad55@gmail.com>
2025-12-11 12:28:06 -0800

    Add CIM practice script

---
+++ hamidCimPractice.ps1
@@ -0,0 +1,7 @@
+﻿Get-CimInstance Win32_ComputerSystem
+Get-CimInstance Win32_ComputerSystem | Format-List name, model, manufacturer
+Get-CimInstance Win32_ComputerSystem | Format-List *
+Get-CimInstance Win32_ComputerSystem | Format-List Cim*
+Get-CimInstance Win32_ComputerSystem | Format-List d*
+Get-CimInstance Win32_ComputerSystem | Format-List d*, t*
+Get-Alias -Definition Get-CimInstance
