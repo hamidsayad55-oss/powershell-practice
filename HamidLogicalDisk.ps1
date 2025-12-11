5f64a4243961d9919f13ee3fbabfaafbf1da9227
Hamid Sayad <hamidsayad55@gmail.com>
2025-12-11 12:32:16 -0800

    Add logical disk analysis script

---
+++ HamidLogicalDisk.ps1
@@ -0,0 +1,15 @@
+﻿gcim Win32_LogicalDisk
+gcim Win32_LogicalDisk -Filter 'DriveType = 3'
+$Disk = Get-CimInstance Win32_LogicalDisk -Filter 'DriveType = 3'
+$Disk
+$Disk = Get-CimInstance Win32_LogicalDisk -Filter 'DriveType = 3'
+$Disk = Get-CimInstance Win32_LogicalDisk -Filter 'DriveType = 3' |
+    Measure-Object -Property FreeSpace -Minimum -Maximum
+$Disk
+$Disk = Get-CimInstance Win32_LogicalDisk -Filter 'DriveType = 3' |
+    Measure-Object -Property FreeSpace -Minimum -Maximum |
+    Select-Object Property, Maximum, Minimum
+$Disk = Get-CimInstance Win32_LogicalDisk -Filter 'DriveType = 3' |
+    Measure-Object -Property FreeSpace -Minimum -Maximum |
+    Select-Object Property, Maximum, Minimum |
+    Format-Table -AutoSize
