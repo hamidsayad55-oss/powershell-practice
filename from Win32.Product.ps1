7168e91acdb0d2d0ca3015db54381276d6bbe485
Hamid Sayad <hamidsayad55@gmail.com>
2025-12-16 09:48:52 -0800

    Add PowerShell DSC scripts

---
+++ from Win32.Product.ps1
@@ -0,0 +1,32 @@
+﻿$Query = "Select * from Win32_Product"
+Write-Host "Counting Installed Products. This may take a little while." -ForegroundColor Blue `n
+Get-CimInstance -Query $Query
+# Start timer
+$dteStart = Get-Date
+
+# Query MSI installed products
+$Query = "Select * from Win32_Product"
+
+Write-Host "Counting Installed Products. This may take a little while." -ForegroundColor Blue `n
+
+Get-CimInstance -Query $Query
+
+# End timer
+$dteEnd = Get-Date
+
+# Compute time difference
+$dteDiff = New-TimeSpan $dteStart $dteEnd
+
+Write-Host "It took $($dteDiff.TotalSeconds) Seconds for this script to complete" -ForegroundColor Yellow
+gcim Win32_Environment
+gcim Win32_Environment | Format-List *
+gcim Win32_Environment | Format-Table Name, VariableValue, UserName
+gcim Win32_Environment | Format-Table Name, VariableValue
+gcim Win32_Environment | Format-Table Name, VariableValue -AutoSize
+Set-Location env:
+sl env:
+Get-ChildItem
+Get-Alias -Definition Invoke-History
+ghy
+Get-History
+sl env:; gci; sl c:\
